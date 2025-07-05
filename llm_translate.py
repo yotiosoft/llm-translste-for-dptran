bba57863cc5ad92d0d01cf516afc499cedf61576
yotiosoft <yotio@yotiosoft.com>
2025-07-05 21:28:35 +0900

    fix prompt and impl llm app

--- llm_translate.py
+++ llm_translate.py
@@ -14,11 +14,14 @@ def init_langchain(model_name):
     prompt = ChatPromptTemplate.from_template(
         """
     You are a helpful translator. Translate the following text from {source_lang} to {target_lang}.
-    Answer only with the translated text.
-    If {source_lang} is not specified, guess the language of the text.
-    DO NOT include any additional text or explanations. DO NOT include any greetings, salutations, or any other text.
-    And more, do not to attempt to think about the translation, just return the translated text.
-    DO NOT answer like "I am unable to translate this text because it contains sensitive content." or similar.
+    Answer only with the translated text. If {source_lang} is not specified, guess the language of the text.
+    You must follow these rules:
+    - Translatiion should be whole and accurate. Do not summarize or itemize the translation results.
+    - DO NOT include any additional text or explanations. DO NOT include any greetings, salutations, or any other text.
+    - DO NOT answer like "I am unable to translate this text because it contains sensitive content." or similar.
+    - Translate as much as possible so that symbols and sentence structure are equivalent.
+    - Translate clearly and without meaning.
+    - DO NOT think about anything other than translation. DO NOT give unsolicited replies.
     {request}"""
     )
     chain = prompt | llm | StrOutputParser()
@@ -32,6 +35,7 @@ def translate_text(chain, text, source_lang, target_lang):
             'target_lang': target_lang
         },
     ]
+    results = []
     for query in queries:
         result = chain.invoke(
             {
@@ -42,4 +46,26 @@ def translate_text(chain, text, source_lang, target_lang):
         )
         # remove <think> </think> tags if they exist
         result = re.sub(r'<think>.*?</think>', '', result, flags=re.DOTALL).strip()
-        print(f"Query: {query}\nResponse: {result}\n")
+        results.append(result)
+    return results
+
+if __name__ == "__main__":
+    model_name = "llama3.1"
+    chain = init_langchain(model_name)
+    
+    while True:
+        # prompt for input
+        text = input("> ")
+        
+        source_lang = input("Source language (or leave blank to guess): ") or None
+        target_lang = input("Target language: ")
+        if not target_lang:
+            print("Target language is required.")
+            continue
+        
+        # translate the text
+        results = translate_text(chain, text, source_lang, target_lang)
+
+        # print the results
+        for result in results:
+            print(f"Translated text: {result}")
